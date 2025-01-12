7d9760e25db991c684c5d212ca75739ea3fd3dfe
Abolfazl Omrani <abolfazl.omrani1999@gmail.com>
2025-01-12 15:27:56 +0330

    conected to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdvertisementsModule } from './advertisements/advertisements.module';
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { CitiesModule } from './city/cities.module';
+import { StatesModule } from './states/states.module';
+import { State } from './states/states.entity';
+import { City } from './city/cities.entity';
 
 @Module({
   imports: [
@@ -10,11 +18,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       host: 'localhost',
       port: 5432,
       username: 'postgres',
-      password: 'admin',
-      database: 'postgres',
+      password: '12011378',
+      database: 'rentify',
       autoLoadEntities: true,
       synchronize: true,
+      entities: [State, City],
     }),
+    AdvertisementsModule,
+    AuthModule,
+    CitiesModule,
+    StatesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
