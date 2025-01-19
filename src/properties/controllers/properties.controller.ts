99307d876eda07ecd6c7de51d79062f32ec1df1a
Abolfazl Omrani <abolfazl.omrani1999@gmail.com>
2025-01-19 15:37:28 +0330

    delete controller added

--- src/properties/controllers/properties.controller.ts
+++ src/properties/controllers/properties.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CreatePropertyDto } from '../dto/create-property.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from 'src/auth/get-user.decorator';
@@ -14,13 +22,19 @@ export class PropertiesController {
     return this.propertiesService.createProperty(body, user);
   }
 
+  @UseGuards(AuthGuard())
+  @Delete(':id')
+  deleteProperty(@Param('id') id: string) {
+    return this.propertiesService.deleteProperty(id);
+  }
+
   @Get()
   getProperties(@GetUser() user: User) {
     return this.propertiesService.getProperties(user);
   }
 
   @Get(':id')
-  getPropertyById(@Param('id') id: number, @GetUser() user: User) {
+  getPropertyById(@Param('id') id: string, @GetUser() user: User) {
     return this.propertiesService.getPropertyById(id, user);
   }
 }
