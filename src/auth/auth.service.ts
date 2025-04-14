5ede279d00927fb616cbb07015af3f1da7cad868
Abolfazl Omrani <abolfazl.omrani1999@gmail.com>
2025-04-14 11:23:29 +0330

    case sensivity for usernname in signin removed

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,6 +6,7 @@ import { SigninDto } from './dto/signin.dto';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { JwtPayload } from './jwt-payload.interface';
+import { ILike } from 'typeorm';
 
 @Injectable()
 export class AuthService {
@@ -37,7 +38,11 @@ export class AuthService {
 
   async signin(signinDto: SigninDto) {
     const { username, password } = signinDto;
-    const user = await this.usersRepository.findOne({ where: { username } });
+    const user = await this.usersRepository.findOne({
+      where: {
+        username: ILike(username),
+      },
+    });
     if (user && (await bcrypt.compare(password, user.password))) {
       const payload: JwtPayload = { username };
       const accessToken = this.jwtService.sign(payload);
